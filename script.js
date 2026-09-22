61bf9f88603dc5868a532643b9724a657d4ddea0
rawkey <qunemt.76@gmail.com>
2026-09-22 22:29:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const ClashMASQUE = document.getElementById('generateButton6');
 const container = document.querySelector('.container');
 
 function generateRandomEndpoint() {
-    const ports = [500, 854, 859, 864, 878, 880, 890, 891, 894, 903, 908, 928, 934, 939, 942, 943, 945, 946, 955, 968, 987, 988, 1002, 1010, 1014, 1018, 1070, 1074, 1180, 1387, 1701, 1843, 2371, 2408, 2506, 3138, 3476, 3581, 3854, 4177, 4198, 4233, 4500, 5279, 5956, 7103, 7152, 7156, 7281, 7559, 8319, 8742, 8854, 8886];
+    const ports = [500, 1701, 2408, 4500];
     
     const selectedServer = getSelectedServer();
     
